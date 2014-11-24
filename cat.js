a795c05421b41efc63cff424773e20b2a4b18142
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2014-11-24 00:55:19 +0000

    rm cat.js

--- cat.js
+++
@@ -1,7 +0,0 @@
-var util  = require('util'),
-    spawn = require('child_process').spawn,
-    ls    = spawn('cat', ['cat.js']);
-
-ls.stdout.on('data', function (data) {
-  console.log('stdout: ' + data);
-});
