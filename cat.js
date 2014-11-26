9b97f5dac7e1184541b681f24a8982892b668bad
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2014-11-26 23:55:38 +0000

    Try build.cson.

--- cat.js
+++
@@ -1,6 +0,0 @@
-var spawn = require('child_process').spawn,
-ls = spawn('cat', ['./nodeGaudi']);
-
-ls.stdout.on('data', function (data) {
-        console.log('stdout: ' + data);
-});
--- exec.js
+++
@@ -1,7 +0,0 @@
-
-var spawn = require('child_process').spawn;
-var ls = spawn('g++', ['blah.cpp', '-o', 'blah']);
-
-ls.stdout.on('data', function (data) {
-        console.log('stdout: ' + data);
-});
