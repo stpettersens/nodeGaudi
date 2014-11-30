cf4aa8fe37fdb81b4759a6c8c1119756721e069e
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2014-11-30 18:34:23 +0000

    Update travis.js

--- travis.js
+++ travis.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var cp = require('child_process');
 var bickle = 'bickle builds stpettersens/nodeGaudi -n 5';
-cp.exec(bickle, function(err, stderr, stdout) {
+cp.exec(bickle, function(err, stdout, stderr) {
 	console.log(stdout);
 });
